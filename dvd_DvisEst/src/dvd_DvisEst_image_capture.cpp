a41a4a782bfe31935b3dd6f7c226169b7ee1564c
bpinkney <bpinkney@aeryon.com>
2020-03-27 15:37:57 -0400

    Maybe fix for opencv on win?

--- dvd_DvisEst/src/dvd_DvisEst_image_capture.cpp
+++ dvd_DvisEst/src/dvd_DvisEst_image_capture.cpp
@@ -7,7 +7,13 @@
 #endif
 
 // OpenCV stuff
-#include <opencv2/opencv.hpp>
+#include "opencv2/core.hpp"
+#include <opencv2/core/utility.hpp>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
 
 #include <stdio.h>
 #include <string.h>
