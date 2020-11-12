0c3e19657834379acf842bd58f5a67cbf9d0493a
bpinkney <bclpinkney@gmail.com>
2020-11-11 20:54:48 -0500

    More test changes to coeffs

--- dvd_DgrafX/Source/DiscVisionDeluxeUE/DfisX/Daero.cpp
+++ dvd_DgrafX/Source/DiscVisionDeluxeUE/DfisX/Daero.cpp
@@ -23,9 +23,9 @@ Also gravity.
 #define CAVITY_EDGE_LIFT_FACTOR (0.00035302903145605) // lift factor as a fucntion of effective exposed cavity edge area inverse
 
 // Pitching moment arms as a percentage of total diameter
-#define PITCHING_MOMENT_FORM_DRAG_PLATE_OFFSET (0.02) // % of diameter toward the front of the disc for plate drag force centre
+#define PITCHING_MOMENT_FORM_DRAG_PLATE_OFFSET (0.1) // % of diameter toward the front of the disc for plate drag force centre
 #define PITCHING_MOMENT_CAVITY_LIFT_OFFSET     (0.05) // % of diameter toward the back of the disc for cavity lift force centre
-#define PITCHING_MOMENT_CAMBER_LIFT_OFFSET     (0.1) // % of diameter toward the front of the disc for camber lift force centre
+#define PITCHING_MOMENT_CAMBER_LIFT_OFFSET     (0.05) // % of diameter toward the front of the disc for camber lift force centre
 #define RIM_CAMBER_EXPOSURE (0.75) // % of lower rim camber exposed to the airflow vs a rim_width * diameter rectangle
 
 
--- dvd_DgrafX/Source/DiscVisionDeluxeUE/DvisEstInterface/DvisEstInterface.cpp
+++ dvd_DgrafX/Source/DiscVisionDeluxeUE/DvisEstInterface/DvisEstInterface.cpp
@@ -173,7 +173,7 @@ void DvisEstInterface::RunDvisEst()
   batFile << "if \"%ERRORLEVEL%\"==\"0\" goto loop" << std::endl;
   if(use_generated_throws)
   {
-    batFile << "\"" + std::string(TCHAR_TO_UTF8(*dVisEst_bin_path)) << "dvd_DvisEst.exe\" -cr -rm=10000 -can=777 -nc 2> nul";
+    batFile << "\"" + std::string(TCHAR_TO_UTF8(*dVisEst_bin_path)) << "dvd_DvisEst.exe\" -cr -rm=20000 -can=10 -nc 2> nul";
   }
   else
   {
